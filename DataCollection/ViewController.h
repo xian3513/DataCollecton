bfceac00eacb51cb2459698bd54c6cb761f18934
kt <kt@a2ndeiMac.local>
2015-01-02 10:08:31 +0800

    Initial Commit

---
+++ DataCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataCollection
+//
+//  Created by kt on 15/1/2.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
